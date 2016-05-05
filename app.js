a0c55f360bca01dd52e8a6d437181b8a80e9030e
zhangjh <njhxzhangjihong@126.com>
2016-05-05 19:51:15 +0800

    frame build

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var mongoose = require("./lib/mongoose");
 
 var app = express();
 
@@ -22,8 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes(mongoose));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var routes = function(mongoose){
+    return function(req,res){
+        //设置一个测试cookie
+        /*res.writeHead(200,{
+            'Set-Cookie': ['myCookie=test','passwd=*****'],
+            'Content-Type': 'text/plan'
+        });
+        */
+        //读取cookie (密码通过加密存储在cookie里:)
+        var cookie = req.cookies;
+        console.log(cookie.passwd);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+        /*
+        mongoose.find("links",{},function(ret){
+            if(ret.length){
+                res.send(ret);
+            }        
+        });
+        */
+        mongoose.find("link",{},function(ret){
+            res.send("index",{
+                title: "favlinks--您的私人收藏夹",ret: ret    
+            });
+        });
+    };
+};
 
-module.exports = router;
+module.exports = routes;
