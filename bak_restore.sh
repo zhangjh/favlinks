6995d050360242beb2519f12b36fa0c306441958
zhangjh <njhxzhangjihong@126.com>
2017-02-03 13:10:45 +0000

    增加数据库备份恢复脚本

---
+++ bak_restore.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+## 定期备份mongodb数据库
+## mongodb启动命令： ./mongod -h xx --port xx --bind_ip xxx --dbpath xx --logpath xx
+
+MODE=$1
+if [ "X$MODE" == "X" ];then
+  MODE="bak"
+fi
+
+MONGO_DIR="/home/`whoami`/mongodb"
+DB_NAME="favlinks"
+DB_PORT="20172"
+BAK_BASE="/home/`whoami`/back/favlinks"
+CURTIME=`date +%Y%m%d`
+BAK_NAME=${BAK_BASE}${CURTIME}
+
+if [ "$MODE" == "bak" ];then
+  echo "开始备份..."
+  cd ${MONGO_DIR}/bin
+  ./mongodump --port ${DB_PORT} -d ${DB_NAME} -o ${BAK_NAME}   
+  if [ $? -ne 0 ];then
+    echo "备份失败"
+    exit 1
+  fi
+  echo "备份成功"
+  exit 0
+fi
+
+if [ "$MODE" == "restore" ];then
+  echo "开始恢复..."
+  cd ${MONGO_DIR}/bin
+  ./mongorestore --port ${DB_PORT} -d ${DB_NAME} --directoryperdb $2
+  if [ $? -ne 0 ];then
+    echo "恢复失败"
+    exit 1
+  fi
+  echo "恢复成功"
+  exit 0
+fi
