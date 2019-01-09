fee99589c473c86eeec25c400c6e17334eb05502
zhangjh <njhxzhangjh@gmail.com>
2019-01-09 23:52:35 +0800

    add navbar

--- antd/src/pages/header.js
+++ antd/src/pages/header.js
@@ -1,13 +1,26 @@
 import { Layout } from 'antd';
-
+import styles from './header.css';
 const { Header } = Layout;
 
 export default (props) => {
     return (
         <Header style={ props.style }>
-            <div style={{ width: '50%'}}>
-                <img alt="logo" src={props.logoImg} style={props.logoImgStyle}/>
-                <img alt="logo2" src={props.logoFont} style={props.logoFontStyle} />
+            <div className={props.navHeader}>
+                <div className={props.logoContainer}>
+                    <img alt="logo" src={props.logoImg} className={props.logoImgClass}/>
+                    <div className={props.titleContainer}>
+                        <p className={props.logoTitleClass}>{props.title}</p>
+                        <p className={props.logoSubTitleClass}>{props.subTitle}</p>
+                    </div>
+                </div>
+                <div className={props.loginContainer}>
+                    您好，<span id={styles.loginUser}>{props.isLogin ? props.loginUser : "游客"}</span>
+                    { props.isLogin ? 
+                        <span className={styles.loginSpan}>退出</span> :
+                        <span className={styles.loginSpan}>登录</span>
+                    }
+                </div>
+                <div className={props.clear}></div>
             </div>
         </Header>
     );
--- antd/src/pages/index.js
+++ antd/src/pages/index.js
@@ -1,4 +1,6 @@
 import Header from './header.js';
+import 'antd/dist/antd.css';
+import styles from './index.css';
 import connect from 'dva';
 
 export default () => {
@@ -6,10 +8,17 @@ export default () => {
         <div style={{ backgroundColor: '#2b7dad' }}>
             <Header
                 style={{ backgroundColor: '#f8f8f8',height: 'auto',margin: '0 auto', maxWidth: '1260px'}}
+                logoContainer={styles.logoContainer}
                 logoImg="https://favlink.cn/img/logo.png"
-                logoImgStyle={{width: '80px'}}
-                logoFont="https://favlink.cn/img/logo-font.png"
-                logoFontStyle={{maxWidth: '54%'}}
+                logoImgClass={styles.logoImg}
+                titleContainer={styles.titleContainer}
+                logoTitleClass={styles.title}
+                logoSubTitleClass={styles.subTitle}
+                title="藏经阁--您的私人定制收藏夹"
+                subTitle="favlink.cn"
+                loginContainer={styles.loginContainer}
+                clear={styles.clear}
+                loginUser="xxxx"
             />
         </div>
     );
