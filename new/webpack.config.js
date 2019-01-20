9164a7fea3044d87bfb28cc0eb567738fcb07aa5
zhangjh <njhxzhangjh@gmail.com>
2019-01-20 14:39:54 +0800

    update

--- new/webpack.config.js
+++ new/webpack.config.js
@@ -1,13 +1,64 @@
-let path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: ['./js/index.jsx'],
+	entry: {
+        index: './src/js/index.jsx'
+    },
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'index.js'
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js'
 	},
 	devServer: {
 		contentBase: './dist',
 		hot: true
-	}
+	},
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                include: [
+                    path.join(__dirname, 'src'),
+                    path.join(__dirname, '/node_modules/antd')
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js|jsx$/,
+                use: [
+                    'babel-loader'
+                ],
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            fileName: "./index.html"
+        })
+    ]
 };
