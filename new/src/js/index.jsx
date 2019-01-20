ff1ae7729087dc06af5cdefea516238c3b135114
zhangjh <njhxzhangjh@gmail.com>
2019-01-20 14:40:51 +0800

    update

---
+++ new/src/js/index.jsx
@@ -0,0 +1,10 @@
+class Hello extends React.Component {
+      render() {
+              return <div>Hello {this.props.toWhat}</div>;
+            }
+}
+
+ReactDOM.render(
+      <Hello toWhat="World" />,
+      document.getElementById('root')
+);
